80c76a4a89d61c1e78c1aa413d8c87091a6d97d1
M1nseoPark <kjms0820@gmail.com>
2023-07-23 12:15:52 +0900

    회원가입/로그인 FE

---
+++ version-2/app/src/main/java/com/minseo/callbank/view/login/Activity_login.kt
@@ -0,0 +1,12 @@
+package com.minseo.callbank.view.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.minseo.callbank.R
+
+class activity_login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
