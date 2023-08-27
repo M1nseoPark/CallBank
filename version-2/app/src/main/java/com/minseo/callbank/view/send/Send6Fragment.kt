26eefc501252ffdd40bd65fca0f5b4807002b392
M1nseoPark <kjms0820@gmail.com>
2023-08-27 21:28:50 +0900

    돈보내기 FE 완료

--- version-2/app/src/main/java/com/minseo/callbank/view/send/Send6Fragment.kt
+++ version-2/app/src/main/java/com/minseo/callbank/view/send/Send6Fragment.kt
@@ -19,10 +19,64 @@ class Send6Fragment : Fragment() {
         val fragmentBinding = FragmentSend6Binding.inflate(inflater, container, false)
         binding = fragmentBinding
 
+        // 금액 입력
+        var amount = ""
+        binding.bt1.setOnClickListener {
+            amount += "1"
+            binding.chAmount.setText(amount+"원")
+        }
+        binding.bt2.setOnClickListener {
+            amount += "2"
+            binding.chAmount.setText(amount+"원")
+        }
+        binding.bt3.setOnClickListener {
+            amount += "3"
+            binding.chAmount.setText(amount+"원")
+        }
+        binding.bt4.setOnClickListener {
+            amount += "4"
+            binding.chAmount.setText(amount+"원")
+        }
+        binding.bt5.setOnClickListener {
+            amount += "5"
+            binding.chAmount.setText(amount+"원")
+        }
+        binding.bt6.setOnClickListener {
+            amount += "6"
+            binding.chAmount.setText(amount+"원")
+        }
+        binding.bt7.setOnClickListener {
+            amount += "7"
+            binding.chAmount.setText(amount+"원")
+        }
+        binding.bt8.setOnClickListener {
+            amount += "8"
+            binding.chAmount.setText(amount+"원")
+        }
+        binding.bt9.setOnClickListener {
+            amount += "9"
+            binding.chAmount.setText(amount+"원")
+        }
+        binding.bt0.setOnClickListener {
+            amount += "0"
+            binding.chAmount.setText(amount+"원")
+        }
+        binding.bt00.setOnClickListener {
+            amount += "00"
+            binding.chAmount.setText(amount+"원")
+        }
+        binding.btDelete.setOnClickListener{
+            if(0 < amount.length) {
+                amount = amount.substring(0, amount.length - 1)
+                binding.chAmount.setText(amount+"원")
+            }
+        }
+
+        // 계속 버튼 클릭
         binding.btNext.setOnClickListener {
             Navigation.findNavController(binding.root).navigate(R.id.action_send6Fragment_to_send7Fragment)
         }
-
+        // 취소 버튼 클릭
         binding.btBack.setOnClickListener {
             Navigation.findNavController(binding.root).navigate(R.id.action_send6Fragment_to_send5Fragment)
         }
